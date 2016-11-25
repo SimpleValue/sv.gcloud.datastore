2e2883008904c59498814cde40a7a02f0f009dff
Maximilian Weber <weber.maximilian@googlemail.com>
2016-11-25 13:00:33 +0100

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sv/gcloud.datastore "0.1.0-SNAPSHOT"
+(defproject sv/gcloud.datastore "0.1.0"
   :description "A client for Google Cloud Datastore."
   :url "https://github.com/SimpleValue/sv.gcloud.storage"
   :license {:name "Eclipse Public License"
